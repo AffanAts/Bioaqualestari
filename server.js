8fbeadc67ccef2edb4be8ef9aa3132f537cd55d3
Affan Haidar Atstsabit <affanhaidar232@gmail.com>
2024-06-14 00:10:45 +0700

    env

--- server.js
+++ server.js
@@ -2,33 +2,35 @@ import { createServer } from 'http';
 import { parse } from 'url';
 import next from 'next';
 
-const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'https://bioaqualestari.vercel.app/'
-const port = process.env.PORT || 3000
+console.log('Next imported:', next); // Tambahkan ini untuk debugging
 
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
       if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
+        await app.render(req, res, '/a', query);
       } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
+        await app.render(req, res, '/b', query);
       } else {
-        await handle(req, res, parsedUrl)
+        await handle(req, res, parsedUrl);
       }
     } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
     }
   }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
