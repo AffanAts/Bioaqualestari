6cd7f3b9abd6353d315e6bc73c64ba40eb1fb9b3
Affan Haidar Atstsabit <affanhaidar232@gmail.com>
2024-06-13 22:44:21 +0700

    change hostname

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { parse } from 'url';
 import next from 'next';
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
+const hostname = 'https://bioaqualestari.vercel.app/'
 const port = process.env.PORT || 3000
 
 const app = next({ dev, hostname, port })
