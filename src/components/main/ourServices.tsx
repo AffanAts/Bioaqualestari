adf083fd613333563b3b57be9da116ba34d4b9c4
Naufal Hadi Chandira <hadinaufal11@gmail.com>
2024-06-14 10:50:01 +0700

    adding data our service from private api

--- src/components/main/ourServices.tsx
+++ src/components/main/ourServices.tsx
@@ -1,8 +1,39 @@
+"use client";
 import React from 'react';
-import {dataServices}  from './page';
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '@/utils/productAPI';
 import Image from 'next/image';
 
-const Services: React.FC = () => {
+interface Product {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+}
+
+export default function ProductsPage () {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const productsData: Product[] = await fetchProducts();
+        if (Array.isArray(productsData)) {
+          setProducts(productsData);
+        } else {
+          throw new Error("Products data is not an array");
+        }
+      } catch (error) {
+        setError((error as Error).message);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+
+
   return (
     <div className="bg-white font-sans min-h-96">
       <div className="max-w-7xl mx-auto">
@@ -12,18 +43,18 @@ const Services: React.FC = () => {
           </h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16 max-md:max-w-lg mx-auto">
-          {dataServices.map(post => (
-            <div key={post.id} className="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative transition-all duration-300 group">
-              <Image width={200} height={200}  src={post.imageUrl} alt={`Blog Post ${post.id}`} className="w-full h-60 object-cover" />
+          {products.map(product => (
+            <div key={product.id} className="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative transition-all duration-300 group">
+              <Image width={200} height={200}  src={product.image} alt={`Blog Post ${product.id}`} className="w-full h-60 object-cover" />
               <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center p-6 text-white text-center">
                 <div>
-                  <h3 className="text-xl font-bold">{post.title}</h3>
+                  <h3 className="text-xl font-bold">{product.name}</h3>
                   <hr className="my-6 border-gray-300" />
-                  <p className="text-sm">{post.description}</p>
+                  <p className="text-sm">{product.description}</p>
                 </div>
               </div>
               <h3 className="text-xl font-bold absolute bottom-4 left-4 z-10 text-white p-2 transition-opacity duration-300 group-hover:opacity-0">
-                {post.title}
+                {product.name}
               </h3>
             </div>
           ))}
@@ -33,4 +64,3 @@ const Services: React.FC = () => {
   );
 };
 
-export default Services;
