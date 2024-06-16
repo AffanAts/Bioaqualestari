0bea917634608879f5f1d58499b9cbc98ca5233d
Affan Haidar Atstsabit <affanhaidar232@gmail.com>
2024-06-16 17:29:49 +0700

    sidebar

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,24 +1,26 @@
-'use client';
+"use client";
 
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { useEffect } from 'react';
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useEffect } from "react";
+import Sidebar from "../components/sidebar";
 
 const Dashboard = () => {
   const { data: session, status } = useSession();
 
   useEffect(() => {
-    if (status === 'unauthenticated') {
+    if (status === "unauthenticated") {
       signIn();
     }
   }, [status]);
 
-  if (status === 'loading') {
+  if (status === "loading") {
     return <div>Loading...</div>;
   }
 
-  if (status === 'authenticated') {
+  if (status === "authenticated") {
     return (
       <div>
+        <Sidebar></Sidebar>
         <div>Welcome, {session?.user?.name}! You are logged in.</div>
         <button onClick={() => signOut()}>Logout</button>
       </div>
