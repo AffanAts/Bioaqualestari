39018d579dbaa8d147885d85e18b0bf50ddc1208
Affan Haidar Atstsabit <affanhaidar232@gmail.com>
2024-06-28 22:29:59 +0700

    Create component invalidImage and tutorialImage

--- src/app/admin/components/blog/updateBlog.tsx
+++ src/app/admin/components/blog/updateBlog.tsx
@@ -3,6 +3,7 @@ import { handleUpdate } from "./updateBlogHandler";
 import dynamic from 'next/dynamic';
 import "react-quill/dist/quill.snow.css"; // Import the styles
 import Image from "next/image";
+import TutorialGambar from "../../../../components/tutorialImage";
 
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
@@ -153,56 +154,7 @@ const ModalUpdateBlog: React.FC<ModalUpdateBlogProps> = ({ blog, onClose }) => {
                     Update blog
                   </button>
                 </div>
-                <div className="col-span-1 pt-5 md:pt-0">
-                  <p className="text-sm text-gray-600 dark:text-gray-400">
-                    <b>Tutorial mendapatkan Link URL gambar</b>
-                  </p>
-                  <ol className="text-sm text-gray-600 dark:text-gray-400 list-decimal list-inside pt-2">
-                    <li>
-                      Kunjungi Website Berikut{" "}
-                      <a
-                        href="https://img.doerig.dev/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500 hover:underline"
-                      >
-                        Klik Disini
-                      </a>{" "}
-                    </li>
-                    <li>
-                      Klik &quot;Upload a file&quot; seperti pada gambar berikut
-                      <center>
-                        {" "}
-                        <div className="relative w-full h-56">
-                          <Image
-                            src="https://i.imgur.com/JcjYQZ6.jpeg"
-                            alt="Gambar"
-                            layout="fill"
-                            objectFit="contain"
-                          />
-                        </div>
-                      </center>
-                    </li>
-                    <li>
-                      Pilihlah gambar yang ingin anda upload dan klik button
-                      &quot;upload&quot;
-                    </li>
-                    <li>
-                      Jika berhasil akan ada tulisan &quot;Success&quot; dan anda bisa
-                      menyalin link gambar tersebut
-                      <center>
-                        <div className="relative w-full h-80">
-                          <Image
-                            alt="Gambar"
-                            layout="fill"
-                            objectFit="contain"
-                            src="https://i.imgur.com/lndMtjB.jpeg"
-                          />
-                        </div>
-                      </center>
-                    </li>
-                  </ol>
-                </div>
+               <TutorialGambar></TutorialGambar>
               </div>
             </form>
           </div>
--- src/app/admin/components/client/updateClient.tsx
+++ src/app/admin/components/client/updateClient.tsx
@@ -1,6 +1,6 @@
 import { useState, ChangeEvent } from "react";
 import { handleUpdate } from "./updateClientHandler";
-import Image from "next/image";
+import Image from "next/image";import TutorialGambar from "../../../../components/tutorialImage";
 
 interface Client {
   id: number;
@@ -122,56 +122,7 @@ const ModalUpdateClient: React.FC<ModalUpdateClientProps> = ({
                     Update client
                   </button>
                 </div>
-                <div className="col-span-1 pt-5 md:pt-0">
-                  <p className="text-sm text-gray-600 dark:text-gray-400">
-                    <b>Tutorial mendapatkan Link URL gambar</b>
-                  </p>
-                  <ol className="text-sm text-gray-600 dark:text-gray-400 list-decimal list-inside pt-2">
-                    <li>
-                      Kunjungi Website Berikut{" "}
-                      <a
-                        href="https://img.doerig.dev/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500 hover:underline"
-                      >
-                        Klik Disini
-                      </a>{" "}
-                    </li>
-                    <li>
-                      Klik &quot;Upload a file&quot; seperti pada gambar berikut
-                      <center>
-                        {" "}
-                        <div className="relative w-full h-56">
-                          <Image
-                            src="https://i.imgur.com/JcjYQZ6.jpeg"
-                            alt="Gambar"
-                            layout="fill"
-                            objectFit="contain"
-                          />
-                        </div>
-                      </center>
-                    </li>
-                    <li>
-                      Pilihlah gambar yang ingin anda upload dan klik button
-                      &quot;upload&quot;
-                    </li>
-                    <li>
-                      Jika berhasil akan ada tulisan &quot;Success&quot; dan anda bisa
-                      menyalin link gambar tersebut
-                      <center>
-                        <div className="relative w-full h-80">
-                          <Image
-                            alt="Gambar"
-                            layout="fill"
-                            objectFit="contain"
-                            src="https://i.imgur.com/lndMtjB.jpeg"
-                          />
-                        </div>
-                      </center>
-                    </li>
-                  </ol>
-                </div>
+                <TutorialGambar></TutorialGambar>
               </div>
             </form>
           </div>
