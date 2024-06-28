22e6e8a9ca215f8a79e2b081e9ed11282d2f96bb
Affan Haidar Atstsabit <affanhaidar232@gmail.com>
2024-06-28 23:20:54 +0700

    new

--- src/components/imageModal.tsx
+++ src/components/imageModal.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import { placeholderImage, isValidUrl } from "./invalidImage";
 
 interface ModalProps {
   isOpen: boolean;
@@ -12,8 +14,21 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, imageUrl }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
       <div className="relative bg-white rounded-lg shadow-lg p-4 w-full max-w-md">
-        <button onClick={onClose} className="absolute top-2 right-2 text-xl font-bold">&times;</button>
-        <img src={imageUrl} alt="Modal Content" className="max-w-full max-h-full" />
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-xl font-bold"
+        >
+          &times;
+        </button>
+        <div className="relative mt-3 min-h-96">
+          <Image
+            src={isValidUrl(imageUrl) ? imageUrl : placeholderImage}
+            alt="Selected"
+            className="rounded-lg"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
       </div>
     </div>
   );
