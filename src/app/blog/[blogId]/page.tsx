a6646df88224174cfcd439215c676ef7f4e5c5c6
Naufal Hadi Chandira <hadinaufal11@gmail.com>
2024-06-28 13:19:08 +0700

    change a new page

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -1,6 +1,8 @@
 "use client"; // Ensure this is at the top
 
+import { insertComment } from "@/utils/commentAPI";
 import React, { useEffect, useState } from "react";
+import Link from "../../../../node_modules/next/link";
 import { fetchBlogById } from "../../../utils/blogAPI";
 
 interface Comment {
@@ -10,7 +12,7 @@ interface Comment {
   name: string;
   created_at: string;
 }
- 
+
 interface Blog {
   id: number;
   title: string;
@@ -84,20 +86,21 @@ export default function BlogDetails({ params }: { params: { blogId: string } }) 
 
   return (
     <>
-      <p className="text-black"><Link href="/blog">Back</Link></p>
+      <p className="text-black">
+        <Link href="/">Back</Link>
+      </p>
       <div key={blog.id} className="container mx-auto p-4 text-black px-60">
         <h1 className="text-4xl font-bold mb-4">{blog.title}</h1>
         <p className="text-gray-600 mb-4">
           By {blog.author} on {new Date(blog.created_at).toLocaleDateString()}
         </p>
-        <img className="w-full h-64 object-cover mb-4" src={blog.image} alt={blog.title} />
+        <img className="w-full h-70 object-cover mb-4" src={blog.image} alt={blog.title} />
         <div className="text-lg" dangerouslySetInnerHTML={{ __html: blog.description }}></div>
       </div>
       <div className="px-20">
         <h1 className="text-black text-3xl font-bold">Related Contents</h1>
-        
       </div>
-      <div className="container p-4 text-black px-60">
+      <div className="container p-4 text-black px-20">
         <h2 className="text-2xl font-bold mb-4">Comments</h2>
         {blog.comments.length > 0 ? (
           blog.comments.map((comment) => (
